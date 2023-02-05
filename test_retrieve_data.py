17be2795ddc36eaf6d5e6f038e4fdf97d574a7fa
Daivaras Slipaitis <slip0001@hz.nl>
2023-02-05 20:45:29 +0100

    minor error on unit test

--- test_retrieve_data.py
+++ test_retrieve_data.py
@@ -8,7 +8,7 @@ class TestRetrieveData(unittest.TestCase):
         mock_requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC%2CETH%2CSOL%2CDOGE&convert=USD&CMC_PRO_API_KEY=4f0a6417-58ab-41f1-8770-6cf42db5d653", 
                           json={"data": {"BTC": {"quote": {"USD": {"price": 12345}}}}}, 
                           status_code=200)
-        result = retrieve_data("USD")
+        result = retrieve_data.retrieve_data("USD")
         self.assertEqual(result, {"BTC": {"price": 12345}})
 
     @requests_mock.Mocker()
@@ -16,7 +16,7 @@ class TestRetrieveData(unittest.TestCase):
         mock_requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC%2CETH%2CSOL%2CDOGE&convert=USD&CMC_PRO_API_KEY=4f0a6417-58ab-41f1-8770-6cf42db5d653", 
                           status_code=500)
         with self.assertRaises(Exception) as context:
-            retrieve_data("USD")
+            retrieve_data.retrieve_data("USD")
         self.assertEqual(str(context.exception), "Error in retrieving data")
 
 if __name__ == '__main__':
