c6183536535a4d88089935506922889d3b1a9072
Daivaras Slipaitis <slip0001@hz.nl>
2023-02-05 20:40:24 +0100

    unit test amended in order to the new code and data fetched

--- test_retrieve_data.py
+++ test_retrieve_data.py
@@ -5,6 +5,7 @@ import retrieve_data
 class TestRetrieveData(unittest.TestCase):
     @requests_mock.Mocker()
     def test_retrieve_data_success(self, mock_requests):
+<<<<<<< Updated upstream
         mock_requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC%2CETH%2CSOL&convert=USD&CMC_PRO_API_KEY=4f0a6417-58ab-41f1-8770-6cf42db5d653",
                           json={
                               "data": {
@@ -42,3 +43,21 @@ class TestRetrieveData(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
+=======
+        mock_requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC%2CETH%2CSOL%2CDOGE&convert=USD&CMC_PRO_API_KEY=4f0a6417-58ab-41f1-8770-6cf42db5d653", 
+                          json={"data": {"BTC": {"quote": {"USD": {"price": 12345}}}}}, 
+                          status_code=200)
+        result = retrieve_data.retrieve_data("USD")
+        self.assertEqual(result, {"BTC": {"price": 12345}})
+
+    @requests_mock.Mocker()
+    def test_retrieve_data_failure(self, mock_requests):
+        mock_requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC%2CETH%2CSOL%2CDOGE&convert=USD&CMC_PRO_API_KEY=4f0a6417-58ab-41f1-8770-6cf42db5d653", 
+                          status_code=500)
+        with self.assertRaises(Exception) as context:
+            retrieve_data.retrieve_data("USD")
+        self.assertEqual(str(context.exception), "Error in retrieving data")
+
+if __name__ == '__main__':
+    unittest.main()
+>>>>>>> Stashed changes
